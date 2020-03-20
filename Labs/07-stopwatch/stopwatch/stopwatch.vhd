231dbcec114360900bbd5dbd41b6f5573ef93a5c
Marek Fiala <60705801+Marxs31@users.noreply.github.com>
2020-03-20 10:35:50 +0100

    Add files via upload

--- Labs/07-stopwatch/stopwatch/stopwatch.vhd
+++ Labs/07-stopwatch/stopwatch/stopwatch.vhd
@@ -3,24 +3,68 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity stopwatch is
+
 port(
-    hth_l_o : out std_logic_vector(4-1 downto 0); 
-    hth_h_o : out std_logic_vector(4-1 downto 0); 
-    sec_h_o : out std_logic_vector(4-1 downto 0); 
-    sec_l_o : out std_logic_vector(4-1 downto 0);
-    clk_i   : in  std_logic;  
-    srst_n_i : in  std_logic;  
-    ce_100Hz_i : in  std_logic; 
-    cnt_en_i : in  std_logic );
-    
-    
+    clk_i       : in std_logic;
+    ce_100Hz_i  : in std_logic;
+    srst_n_i    : in std_logic;
+    cnt_en_i    : in std_logic; 
+  
+    sec_h_o     : out unsigned (3 downto 0);
+    sec_l_o     : out unsigned (3 downto 0);
+    hth_h_o     : out unsigned (3 downto 0);
+    hth_l_o     : out unsigned (3 downto 0)
+	);
+end entity stopwatch;
 
 architecture Behavioral of stopwatch is
-    signal s_cnt0 : unsigned(4-1 downto 0) := (others => '0');
-   
+    
+	signal cnt_sec_h_o  : unsigned (3 downto 0) := (others => '0');
+	signal cnt_sec_l_o  : unsigned (3 downto 0) := (others => '0');
+	signal cnt_hth_h_o  : unsigned (3 downto 0) := (others => '0');
+	signal cnt_hth_l_o  : unsigned (3 downto 0) := (others => '0');
+    signal cnt_en		: std_logic;
 
-    s_cnt0 <= s_cnt0 + 1;
- 
-
-    hth_l_o <= std_logic_vector(s_cnt0);
+begin
+     
+    p_assign: process(clk_i)
+    begin
+    	if rising_edge(clk_i) then 
+        	hth_l_o <= unsigned(cnt_hth_l_o);
+            hth_h_o <= unsigned(cnt_hth_h_o);
+            sec_l_o <= unsigned(cnt_sec_l_o);
+            sec_h_o <= unsigned(cnt_sec_h_o);
+            if cnt_en_i = '1' then
+            	cnt_en 	<=	'0';
+            else
+            	cnt_en 	<=	'1';
+            end if;
+        end if;
+      end process; 
+            
+    p_cnt: process(clk_i, srst_n_i, cnt_en, cnt_sec_h_o, cnt_sec_l_o, cnt_hth_h_o, cnt_hth_l_o)
+    begin    
+		if rising_edge(clk_i) then    	    
+        	if srst_n_i = '0' or (cnt_hth_l_o = "1001" and cnt_hth_h_o = "1001" and cnt_sec_l_o = "1001" and cnt_sec_h_o = "0101") then
+            	cnt_sec_h_o <= (others => '0');  
+         	    cnt_sec_l_o <= (others => '0');
+        	    cnt_hth_h_o <= (others => '0');
+         	    cnt_hth_l_o <= (others => '0');
+       	 	elsif cnt_en = '1' and ce_100Hz_i = '1' then
+        		cnt_hth_l_o <= cnt_hth_l_o + 1;
+        	    if cnt_hth_l_o = "1001" then
+					cnt_hth_l_o <= (others => '0');
+		   	       	cnt_hth_h_o <= cnt_hth_h_o + 1;
+                   	if cnt_hth_h_o = "1001" then
+						cnt_hth_h_o <= (others => '0');
+        	        	cnt_sec_l_o <= cnt_sec_l_o + 1;
+            	        if cnt_sec_l_o = "1001" then
+                    		cnt_sec_l_o <= (others => '0');
+                        	cnt_sec_h_o <= cnt_sec_h_o + 1;
+                        end if;
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process; 
 end architecture Behavioral;
