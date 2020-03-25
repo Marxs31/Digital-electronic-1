6c1b523d3dc69ec9464af9c18ca5c7439e58ca25
Marek Fiala <60705801+Marxs31@users.noreply.github.com>
2020-03-25 10:50:16 +0100

    Update clock_enable.vhd

--- Labs/07-stopwatch/stopwatch/clock_enable.vhd
+++ Labs/07-stopwatch/stopwatch/clock_enable.vhd
@@ -1,4 +1,4 @@
------------------------------------------------------------------------
+------------------------------------------------------------------------
 --
 -- Generates clock enable signal.
 -- Xilinx XC2C256-TQ144 CPLD, ISE Design Suite 14.7
@@ -11,28 +11,27 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
---use ieee.std_logic_unsigned.all;    
+use ieee.std_logic_unsigned.all;    -- Provides unsigned numerical computation
 
 ------------------------------------------------------------------------
 -- Entity declaration for clock enable
 ------------------------------------------------------------------------
 entity clock_enable is
 generic (
-    g_NPERIOD : unsigned(16-1 downto 0) := x"0004"
+    g_NPERIOD : std_logic_vector(16-1 downto 0) := x"0004"
 );
 port (
     clk_i          : in  std_logic;
-    srst_n_i       : in  std_logic; 
+    srst_n_i       : in  std_logic; -- Synchronous reset (active low)
     clock_enable_o : out std_logic
-   );
+);
 end entity clock_enable;
 
 ------------------------------------------------------------------------
 -- Architecture declaration for clock enable
 ------------------------------------------------------------------------
 architecture Behavioral of clock_enable is
-    signal s_cnt : unsigned(16-1 downto 0) := x"0000";
+    signal s_cnt : std_logic_vector(16-1 downto 0) := x"0000";
 begin
 
     --------------------------------------------------------------------
@@ -41,11 +40,11 @@ begin
     -- domain. By default enable signal is low and generated pulse is 
     -- always one clock long.
     --------------------------------------------------------------------
-    p_clk_enable : process (clk_i)
+    p_clk_enable : process(clk_i)
     begin
-        if rising_edge(clk_i) then  
-            if srst_n_i = '0' then  
-                s_cnt <= (others => '0');   
+        if rising_edge(clk_i) then  -- Rising clock edge
+            if srst_n_i = '0' then  -- Synchronous reset (active low)
+                s_cnt <= (others => '0');   -- Clear all bits
                 clock_enable_o <= '0';
             else
                 if s_cnt >= g_NPERIOD-1 then
