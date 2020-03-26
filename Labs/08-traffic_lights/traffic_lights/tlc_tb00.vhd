88a9ef1ee4761f94b9a53dd3610f847398cb3f40
Marek Fiala <60705801+Marxs31@users.noreply.github.com>
2020-03-26 11:34:14 +0100

    Add files via upload

---
+++ Labs/08-traffic_lights/traffic_lights/tlc_tb00.vhd
@@ -0,0 +1,65 @@
+--------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+--use ieee.numeric_std.all;
+ 
+entity tlc_tb00 is
+end tlc_tb00;
+ 
+architecture behavior of tlc_tb00 is 
+ 
+    component traffic_light_controller
+    port(
+         clk_i : IN  std_logic;
+ 	 ce_2HZ_i : IN  std_logic;
+         srst_n_i : IN  std_logic;
+         lights_o : OUT  std_logic_vector(5 downto 0)
+        );
+    end component;
+    
+   --Vstupy
+   signal clk_i : std_logic := '0';
+   signal ce_2HZ_i : std_logic := '0';
+   signal srst_n_i : std_logic := '0';
+ 
+   --Vystupy
+   signal lights_o : std_logic_vector(5 downto 0);
+
+   -- Clock period definitions
+   constant clk_i_period : time := 10 ns;
+   begin
+ 
+   uut: traffic_light_controller port map (
+          clk_i => clk_i,
+     	  ce_2HZ_i => ce_2HZ_i,
+          srst_n_i => srst_n_i,
+          lights_o => lights_o
+        );
+
+   -- Clock process definitions
+   clk_i_process :process
+   begin
+		clk_i <= '0';
+		wait for clk_i_period/2;
+		clk_i <= '1';
+		wait for clk_i_period/2;
+   end process;
+ 
+	ce_2Hz_i_process :process
+   begin
+		ce_2Hz_i <= '0';
+		wait for clk_i_period;
+		ce_2Hz_i <= '1';
+		wait for clk_i_period;
+   end process;
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+		srst_n_i <= '1';
+		wait for 600 ns;
+		srst_n_i <= '0';	
+      wait;
+   end process;
+end;
+--Fiala Marek
---
+++ Labs/08-traffic_lights/traffic_lights/traffic_light_controller.vhd
@@ -0,0 +1,115 @@
+library ieee
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity traffic_light_controller is
+port (
+	clk_i 	: in std_logic;  --10 kHz clock
+	srst_n_i : in std_logic; --reset
+	ce_2HZ_i : in std_logic; --2 Hz for the counter
+	
+	lights_o : out std_logic_vector (6-1 downto 0); -- LED output 
+	count_o 	: out std_logic_vector (4-1 downto 0) --for tb01 
+);
+	
+end  entity traffic_light_controller;
+
+architecture Behavioral of traffic_light_controller is
+	type state_type is 		(c_c0, c_ze, c_zl, c_c1, ze_c, zl_c);
+--		s0	 	s1	   	s2		s3			s4			s5
+-- s0 c_c0 in case of reset 
+	signal s_state: state_type;
+	signal s_count: unsigned (4-1 downto 0);
+	constant c_5sec : unsigned (4-1 downto 0) := "1001";  -- 9 (dec) (0 to 9)
+	constant c_1sec : unsigned (4-1 downto 0) := "0001";  -- 1 (dec) (0 to 1)
+
+begin
+	p_change_state : process(clk_i)
+	begin
+		if rising_edge(clk_i) then
+			if srst_n_i = '0' then  --reset
+				s_state <= rd_rd0;  -- state 0 
+				s_count <= (others => '0');
+			
+			elsif ce_2Hz_i = '1' then
+				case s_state is
+					when c_c0 =>   					--s0
+						if s_count < c_1sec then		--wait 1s
+							s_state <= c_c0; 		--s0
+							s_count <= s_count + 1;
+						else
+							s_state <= c_ze; 		--s1
+							s_count <= (others => '0'); 	--set to 0
+						end if;
+						
+					when c_ze =>					--s1
+						if s_count < c_5sec then		--wait 5s
+							s_state <= c_ze; 		--s1
+							s_count <= s_count + 1;
+						else
+							s_state <= c_zl; 		--s2
+							s_count <= (others => '0'); 	--set to 0
+						end if; 
+							
+					when c_zl =>					--s2
+						if s_count < c_1sec then		--wait 1s
+							s_state <= c_zl; 		--s2
+							s_count <= s_count + 1;
+						else
+							s_state <= c_c1; 		--s3
+							s_count <= (others => '0'); 	--set to 0
+						end if; 
+						
+					when c_c1 =>					--s3
+						if s_count < c_1sec then		--wait 1s
+							s_state <= c_c1; 		--s3
+							s_count <= s_count + 1;
+						else
+							s_state <= ze_c; 		-- s4
+							s_count <= (others => '0'); 	--set to 0
+						end if; 
+						
+					when ze_c =>					--s4
+						if s_count < c_5sec then		--wait 5s
+							s_state <= ze_c; 		--s4
+							s_count <= s_count + 1;
+						else
+							s_state <= zl_c; 		--s5
+							s_count <= (others => '0'); 	--set to 0
+						end if; 
+						
+					when zl_c =>					--s5
+						if s_count < c_1sec then		--wait 1s
+							s_state <= zl_c; 		--s5
+							s_count <= s_count + 1;
+						else
+							s_state <= c_c0; 		--s0
+							s_count <= (others => '0'); 	--set to 0
+						end if; 
+						
+					when others =>
+							s_state <= c_c0; 		--s0
+							
+				end case;
+			end if;
+		end if;
+	end process;
+	
+	--states: c_c0, c_ze, c_zl, c_c1, ze_c, zl_c
+	-- lights_o : "Zelena Zluta Cervena Zelena Zluta Cervena"
+	p_out : process(s_state)
+		begin
+		case s_state is
+			when c_c0 => lights_o <= "001001"; 
+			when c_ze => lights_o <= "001100"; 
+			when c_zl => lights_o <= "001010"; 
+			when c_c1 => lights_o <= "001001"; 
+			when ze_c => lights_o <= "100001"; 
+			when zl_c => lights_o <= "010001"; 
+			when others => lights_o <= "001001";
+		end case;
+	end process;
+	
+	count_o <= std_logic_vector(s_count); -- tb01
+	
+end architecture Behavioral;
